340a5397fbbc1ae7090450575a9bf309ab3460df
Gabriel Jospin <gabriel.jospin@usp.br>
2023-06-15 15:20:40 -0300

    Initial commit

---
+++ src/main/java/br/usp/josin/university_admin/UniversityAdminApplication.java
@@ -0,0 +1,13 @@
+package br.usp.josin.university_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/usp/josin/university_admin/UniversityAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usp.josin.university_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
